40d9cb242b601712a30eb3c8f44554768e0a9b8b
KaioPinto <kaioricardo76@gmail.com>
2024-06-21 13:01:05 -0300

    alterações vit config

--- frontend/src/axios/config.js
+++ frontend/src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const partyFetch = axios.create({
-  baseURL: "http://18.117.148.17:3000/api/",
+  baseURL: "http://3.17.156.116:3000/api/",
   headers: {
     "Content-Type": "application/json",
   },
