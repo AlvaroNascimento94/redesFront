39e01c30b2f6cdad7fe2b1c856e2d21544ade8ad
KaioPinto <kaioricardo76@gmail.com>
2024-06-22 00:02:04 -0300

    alterações de conexao

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'ec2-18-189-141-54.us-east-2.compute.amazonaws.com', 
+    host: true, 
     port: 5173, 
   }
 })
