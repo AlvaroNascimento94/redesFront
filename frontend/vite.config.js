8ec19abd46f18efdaa4fb1db8ff74b943ccbf1e4
KaioPinto <kaioricardo76@gmail.com>
2024-06-21 22:53:24 -0300

    alterações de vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, 
+    host: 'ec2-18-189-141-54.us-east-2.compute.amazonaws.com', 
     port: 5173, 
   }
 })
