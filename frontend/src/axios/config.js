83849fcb5033c867628a3fff7ec514ef3af51715
KaioPinto <kaioricardo76@gmail.com>
2024-06-21 22:48:43 -0300

    alterações de axios

--- frontend/src/axios/config.js
+++ frontend/src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const partyFetch = axios.create({
-  baseURL: "http//:ec2-18-224-51-237.us-east-2.compute.amazonaws.com:3000/api/",
+  baseURL: "http://ec2-18-224-51-237.us-east-2.compute.amazonaws.com:3000/api/",
   headers: {
     "Content-Type": "application/json",
   },
