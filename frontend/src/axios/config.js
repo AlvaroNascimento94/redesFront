bd08299cc0d18275a1738697b80180f83ce84cc7
Alvaro Augusto do Nascimento Filho <alvarosum@hotmail.com>
2024-06-25 09:40:31 -0300

    alteracao conexao

--- frontend/src/axios/config.js
+++ frontend/src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const partyFetch = axios.create({
-  baseURL: "http://18.227.111.236:3000/api/",
+  baseURL: "http://18.227.111.194:3000/api/",
   headers: {
     "Content-Type": "application/json",
   },
